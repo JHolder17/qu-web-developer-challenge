ec2abc1c5788ad356f4a84acb2504f708a23d5e0
jholder17 <j.holder17@gmail.com>
2025-06-28 16:08:47 -0400

    file resolver pkg installed

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import Components from 'unplugin-vue-components/vite';
+import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      dirs: ['src/components'],
+      extensions: ['vue'],
+      deep: true,
+      dts: true,
+      resolvers: [
+        PrimeVueResolver()
+      ]
+    }),
     vueDevTools(),
     tailwindcss(),
   ],
