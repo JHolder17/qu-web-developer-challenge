6cf62638625278807cf720faeddec25e475aa04b
jholder17 <j.holder17@gmail.com>
2025-06-29 16:23:16 -0400

    rollback of all gh-pages changes

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import Components from 'unplugin-vue-components/vite';
 import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 
 export default defineConfig({
-  base: '/qu-web-developer-challenge/',
   plugins: [
     vue(),
     Components({
