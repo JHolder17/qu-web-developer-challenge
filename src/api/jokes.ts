62a2b8834e9e3977caf531b809bfa375a07b8539
jholder17 <j.holder17@gmail.com>
2025-06-29 21:55:06 -0400

    removed unused api call

--- src/api/jokes.ts
+++ src/api/jokes.ts
@@ -2,11 +2,6 @@ import axios from 'axios';
 
 const API_BASE = 'https://official-joke-api.appspot.com';
 
-export const getRandomJoke = async () => {
-  const res = await axios.get(`${API_BASE}/random_joke`);
-  return res.data;
-};
-
 export const getOneHundredJokes = async () => {
   const res = await axios.get(`${API_BASE}/jokes/random/100`);
   return res.data;
