774e2ebff85c9f4d0a21d679889b7e8a56a2be66
jholder17 <j.holder17@gmail.com>
2025-06-30 13:30:00 -0400

    updated vite config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import Components from 'unplugin-vue-components/vite';
 import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 
 export default defineConfig({
+  base: '/qu-web-developer-challenge/',
   plugins: [
     vue(),
     Components({
